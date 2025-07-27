655a869532726d316a4bd367ec5454bbcc46b836
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2025-07-26 19:43:51 -0500

    Improve launch scripts, add service example for systemd and launchd
    Signed-off-by: Mitch Gaffigan <mitch.gaffigan@comcast.net>

--- server/basedir-includes/oieserver.bat
+++ server/basedir-includes/oieserver.bat
@@ -1,7 +1,25 @@
 @echo off
 
-java @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher %*
+REM Set the script directory as the working directory
+cd %~dp0
+
+REM Use the local jre, JAVA_HOME environment variable, or default java command.
+SET "JAVA_CMD=java"
+IF DEFINED JAVA_HOME SET "JAVA_CMD=%JAVA_HOME%\bin\java"
+IF EXIST "jre\bin\java.exe" SET "JAVA_CMD=jre\bin\java"
+
+REM Run the Java command with the specified arguments
+"%JAVA_CMD%" @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher %*
+
 REM if exit code == 75, then the arguments changed and we need to restart
 IF %ERRORLEVEL% EQU 75 (
-    CALL "%~f0" %*
+    "%JAVA_CMD%" @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher %*
 )
+
+REM if exit code != 0, maybe wrong version of Java?
+IF NOT %ERRORLEVEL% == 0 (
+    ECHO ERROR %ERRORLEVEL%: Failed to start Mirth Connect using Java '%JAVA_CMD%'.>&2
+    ECHO Please ensure %JAVA_HOME% refers to Java 17 or later.>&2
+)
+
+EXIT /B %ERRORLEVEL%
--- server/basedir-includes/oieserver.sh
+++ server/basedir-includes/oieserver.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-java @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher "$@"
+# Set the script directory as the working directory
+cd "$(dirname -- "$0")" || exit 1
+
+# Use the local jre, JAVA_HOME environment variable, or default java command.
+JAVA_CMD="java"
+[ -n "${JAVA_HOME}" ] && JAVA_CMD="${JAVA_HOME}/bin/java"
+[ -f "jre/bin/java" ] && JAVA_CMD="jre/bin/java"
+
+# Run the Java command with the specified arguments
+"$JAVA_CMD" @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher "$@"
+
 # if exit code == 75, then the arguments changed and we need to restart
 if [ $? -eq 75 ]; then
-    exec "$0" "$@"
+    "$JAVA_CMD" @appdata/launch.args com.mirth.connect.server.launcher.VmOptionsLauncher "$@"
 fi
+
+# if exit code == 1, maybe wrong version of Java?
+if [ $? -ne 0 ]; then
+    echo "ERROR $?: Failed to start Mirth Connect using Java '$JAVA_CMD'." >&2
+    echo "Please ensure JAVA_HOME refers to Java 17 or later." >&2
+fi
+
+exit $?
