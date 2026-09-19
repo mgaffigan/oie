b8b0467317e0b840a8f3b8dad00df94b0a1c1c4a
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2026-09-19 17:07:05 -0500

    Re-add EDISerializerTests using new fixtures
    Signed-off-by: Mitch Gaffigan <mitch.gaffigan@comcast.net>

--- server/src/test/java/com/mirth/connect/plugins/datatypes/edi/EDISerializerTest.java
+++ server/src/test/java/com/mirth/connect/plugins/datatypes/edi/EDISerializerTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
 import com.mirth.connect.donkey.model.message.MessageSerializerException;
+import com.mirth.connect.model.converters.TestUtil;
 import com.mirth.connect.model.datatype.SerializerProperties;
 
 public class EDISerializerTest {
@@ -41,6 +45,36 @@ public class EDISerializerTest {
 		assertTrue(exceptionCaught);
 	}
 	
+	@Test
+	public void testToXml() throws Exception {
+		assertXmlEquals(read("test-edi-output.xml"), serializer.toXML(read("test-edi-input.txt")));
+	}
+
+	@Test
+	public void testFromXml() throws Exception {
+		assertEquals(read("test-edi-input.txt"), serializer.fromXML(read("test-edi-output.xml")));
+	}
+
+	/** Issue 1597: the serializer fills in elements the XML leaves out. */
+	@Test
+	public void testIssue1597fromXML() throws Exception {
+		assertEquals(read("test-1597-output.txt"), serializer.fromXML(read("test-1597-input-missing-elements.xml")));
+	}
+
+	@Test
+	public void testIssue1597toXML() throws Exception {
+		assertXmlEquals(read("test-1597-input.xml"), serializer.toXML(read("test-1597-output.txt")));
+	}
+
+	private static String read(String name) throws Exception {
+		return FileUtils.readFileToString(new File("tests/" + name), "UTF-8");
+	}
+
+	/** toXML returns one unindented line with an XML declaration; the fixtures are readable. */
+	private static void assertXmlEquals(String expected, String actual) throws Exception {
+		assertEquals(TestUtil.normalizeLineEndings(expected), TestUtil.normalizeLineEndings(TestUtil.prettyPrintXml(actual)));
+	}
+
 	private static String validXml =  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\r\n"
 			+ "<foo><bar>bar</bar></foo>";
 	
